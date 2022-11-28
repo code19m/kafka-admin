f7094efcdf1db57474884e46e9350d36b6c25181
Nuriddin-Olimjon <Nuriddin.Obidjonov777@gmail.com>
2022-11-28 15:43:09 +0500

    init

---
+++ consumer.py
@@ -0,0 +1,59 @@
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Consumer, OFFSET_BEGINNING
+
+
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    parser.add_argument('topic_name')
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+    config.update(config_parser['consumer'])
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+    # Set up a callback to handle the '--reset' flag.
+    def reset_offset(consumer, partitions):
+        if args.reset:
+            for p in partitions:
+                p.offset = OFFSET_BEGINNING
+            consumer.assign(partitions)
+
+    # Subscribe to topic
+    topic = args.topic_name
+    consumer.subscribe([topic], on_assign=reset_offset)
+
+    # Poll for new messages from Kafka and print them.
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("...")
+            elif msg.error():
+                raise msg.error()
+            else:
+                # Extract the (optional) key and value, and print.
+
+                print(
+                    # f"TOPIC: {msg.topic()}\n"
+                    # f"KEY: {msg.key()}\n"
+                    f"VALUE: {msg.value()}\n"
+                )
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
